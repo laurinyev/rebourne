59b1798a178efdb7aaea638daa0fda25d050917f
laurinyev <retek1544@gmail.com>
2026-03-28 09:44:06 +0100

    added \W, \v and \Vq

--- src/repl.rs
+++ src/repl.rs
@@ -26,6 +26,30 @@ fn get_curr_dir() -> String {
     }
 }
 
+fn get_curr_dir_basename() -> String {
+    let dir = current_dir()
+                .expect("Failed to get current directory")
+                .file_name()
+                .expect("Failed to get the basename of the current directory")
+                .to_str()
+                .expect("Failed to convert current directory basename to string")
+                .to_string();
+
+    dir
+}
+
+fn get_version_string(patchlevel: bool) -> String {
+    let maj   = env!("CARGO_PKG_VERSION_MAJOR");
+    let min   = env!("CARGO_PKG_VERSION_MINOR");
+    let patch = env!("CARGO_PKG_VERSION_PATCH");
+
+    if patchlevel {
+        format!("{}.{}.{}", maj, min, patch)
+    } else {
+        format!("{}.{}", maj, min)
+    }
+}
+
 fn process_prompt(raw: &str) -> String{
     let mut buffer = String::new();
     let mut parse_mode = PS1ParseMode::Normal;
@@ -43,6 +67,9 @@ fn process_prompt(raw: &str) -> String{
             PS1ParseMode::Escape => {
                 match c {
                    'w' => buffer.push_str(&get_curr_dir()), 
+                   'W' => buffer.push_str(&get_curr_dir_basename()), 
+                   'v' => buffer.push_str(&get_version_string(false)), 
+                   'V' => buffer.push_str(&get_version_string(true)), 
                     _ => buffer.push(c)
                 }
                 parse_mode = PS1ParseMode::Normal;
